2e75cf863bfe204c21013a9cba4ca1ad8db43338
adarve <ruben.adarveee@gmail.com>
2022-12-03 11:57:34 +0100

    Testeando Copilot

--- Ejercicio1/Kids/src/Kingergarden.java
+++ Ejercicio1/Kids/src/Kingergarden.java
@@ -1,5 +1,8 @@
-public class App {
+import java.util.concurrent.Semaphore;
+
+public class Kingergarden {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
 }
